c6d2f5b5ff7ba1eb4d712d2ab8a3192e14c5e57d
Prayag Sharma <85249351+prg2308@users.noreply.github.com>
2022-06-15 15:30:44 +0530

    Cors fix attempt

--- server.js
+++ server.js
@@ -3,25 +3,27 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const path = require('path')
 const mongoose = require('mongoose')
 
 const port = process.env.PORT || 8080
 
-//Since a remote atlas server has to be configured, this is left for later
-// mongoose.connect('mongodb://localhost:27017/<dbname>', { useNewUrlParser: true, useUnifiedTopology: true })
-//     .then(() => {
-//         console.log('Connected to mongod')
-//     })
-//     .catch((err) => {
-//         console.log('Connection Error', err);
-//     })
+
+mongoose.connect('mongodb://localhost:27017/docserver', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to mongod')
+    })
+    .catch((err) => {
+        console.log('Connection Error', err);
+    })
 
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.status(200).json({ status: 'OK', message: 'If you are seeing this, server works!' });
